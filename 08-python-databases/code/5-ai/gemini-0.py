a42c7cad18cba35193faac251fdd9fa29028b506
Erel Segal-Halevi <erelsgl@gmail.com>
2026-04-09 18:19:53 +0300

    edit database code samples

---
+++ 08-python-databases/code/5-ai/gemini-0.py
@@ -0,0 +1,21 @@
+# pip install google-genai
+from google import genai
+
+# The client gets the API key from the environment variable `GEMINI_API_KEY`.
+client = genai.Client()
+
+# print("List of models that support generateContent:\n")
+# for m in client.models.list():
+#     for action in m.supported_actions:
+#         if action == "generateContent":
+#             print(m.name)
+
+model = "gemini-flash-lite-latest"
+try:
+    response = client.models.generate_content(
+        model=model,
+        contents="Invent an original joke that non-English speakers could understand"
+    )
+    print(response.text)
+except genai.errors.ServerError as err:
+    print(f"Model {model} error: {err}")
