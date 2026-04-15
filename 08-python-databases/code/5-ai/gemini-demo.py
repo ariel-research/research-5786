65babaf2bf065b238c5a877837d8f43e9f35cac9
Erel Segal-Halevi <erelsgl@gmail.com>
2026-04-15 17:49:32 +0300

    ai demo

--- 08-python-databases/code/5-ai/gemini-demo.py
+++ 08-python-databases/code/5-ai/gemini-demo.py
@@ -4,11 +4,11 @@ from google import genai
 # The client gets the API key from the environment variable `GEMINI_API_KEY`.
 client = genai.Client()
 
-# print("List of models that support generateContent:\n")
-# for m in client.models.list():
-#     for action in m.supported_actions:
-#         if action == "generateContent":
-#             print(m.name)
+print("List of models that support generateContent:\n")
+for m in client.models.list():
+    for action in m.supported_actions:
+        if action == "generateContent":
+            print(m.name)
 
 model = "gemini-flash-lite-latest"
 try:
